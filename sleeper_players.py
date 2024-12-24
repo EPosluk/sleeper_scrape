ccfd573a971759393c37d21997069a3f5632511d
eposluk <52364323+EPosluk@users.noreply.github.com>
2024-12-23 21:42:02 -0500

    Initial sleeper_players creation

---
+++ sleeper_players.py
@@ -0,0 +1,36 @@
+import requests
+import pandas as pd
+import json
+
+def main():
+    ######################
+    #       SET UP       #
+    ######################
+    
+
+    ######################
+    #       EXTRACT      #
+    ######################
+    # Get Players Data via Sleeper API
+    url = 'https://api.sleeper.app/v1/players/nfl'
+    response = requests.get(url)
+
+
+    ######################
+    #     TRANSFORM      #
+    ######################
+    # Flatten nested dictionary to a dataframe
+    json_data = json.loads(response.content)
+    players = [{**{'player_id':k1}, **v1} for k1, v1 in json_data.items()]
+    players_df = pd.DataFrame(players)
+
+
+    ######################
+    #        LOAD        #
+    ######################
+    # Write players dataframe to postgres db
+
+
+url = 'https://api.sleeper.app/v1/players/nfl'
+response = requests.get(url)
+
