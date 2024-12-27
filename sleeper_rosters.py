7cc1916005e679c94a7afca8451ea1335dc99c17
eposluk <52364323+EPosluk@users.noreply.github.com>
2024-12-27 17:22:17 -0500

    Updates to rosters py file. Pulls and flattens required fields

--- sleeper_rosters.py
+++ sleeper_rosters.py
@@ -6,23 +6,31 @@ def main():
     ######################
     #       SET UP       #
     ######################
-    
+    # Subsets of keys and child keys to extract
+    keys_subset = ['league_id', 'players', 'reserve', 'roster_id', 'starters', 'taxi'] 
+    metadata_subset = ['record', 'streak']
+    settings_subset = ['division', 'fpts', 'fpts_against', 'losses', 'ppts', 'ties', 'waiver_budget_used', 'waiver_position', 'wins']
+    rosters_df = pd.DataFrame(columns=keys_subset + metadata_subset + settings_subset) # Create empty dataframe with columns of interest
+    rosters_dict = {}
 
     ######################
     #       EXTRACT      #
     ######################
-    # Get Players Data via Sleeper API
-    url = 'https://api.sleeper.app/v1/league/1048244634536857600/rosters'
+    url = 'https://api.sleeper.app/v1/league/964039299760967680/rosters'
     response = requests.get(url)
-
 
     ######################
     #     TRANSFORM      #
     ######################
     # Flatten nested dictionary to a dataframe
     json_data = json.loads(response.content)
-    players = [{**{'player_id':k1}, **v1} for k1, v1 in json_data.items()]
-    players_df = pd.DataFrame(players)
+    for roster in json_data:
+        keys_dict = {k: roster[k] for k in keys_subset}
+        metadata_dict = {k: roster['metadata'][k] for k in metadata_subset}
+        settings_dict = {k: roster['settings'][k] for k in settings_subset}
+        rosters_dict = keys_dict | metadata_dict | settings_dict
+        row_df = pd.DataFrame([rosters_dict])
+        rosters_df = pd.concat([rosters_df, row_df], axis=0, ignore_index = True)
     
 
 
