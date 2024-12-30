31ac3a5c949ade6c83d6332eeacdc41bb7d08762
eposluk <52364323+EPosluk@users.noreply.github.com>
2024-12-30 11:20:19 -0500

    Sleeper state file creation

---
+++ sleeper_state.py
@@ -0,0 +1,41 @@
+import requests
+import pandas as pd
+import json
+
+def main():
+    ######################
+    #       SET UP       #
+    ######################
+    
+
+    ######################
+    #       EXTRACT      #
+    ######################
+    # Get Users Data via Sleeper API
+    url = 'https://api.sleeper.app/v1/state/nfl'
+    response = requests.get(url)
+
+
+    ######################
+    #     TRANSFORM      #
+    ######################
+    # Extract data of interest using subset of key/value pairs
+    keys_subset = ['week', # week
+                    'season', # current season
+                    'season_type', # pre, post, regular
+                    'season_start_date', # regular season start
+                    'leg', # week of regular season
+                    'league_season', # active season for leagues
+                    'league_create_season'] # flips in December
+    state_df = pd.DataFrame([{k: response.json()[k] for k in keys_subset}])
+    
+    
+
+
+    ######################
+    #        LOAD        #
+    ######################
+    # Write players dataframe to postgres db
+
+
+
