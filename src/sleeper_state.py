e07ac07122390b219d167a242649493572ab496a
eposluk <52364323+EPosluk@users.noreply.github.com>
2024-12-31 14:45:55 -0500

    Update sleeper_state.py to write to local postgres db

--- src/sleeper_state.py
+++ src/sleeper_state.py
@@ -1,12 +1,17 @@
 import requests
 import pandas as pd
-import json
+import psycopg
+import config
+from sqlalchemy import create_engine
+
 
 def main():
     ######################
     #       SET UP       #
     ######################
-    
+    # SQL Alchemy engine creation
+    engine = create_engine(config.connection_string)
+
 
     ######################
     #       EXTRACT      #
@@ -28,14 +33,15 @@ def main():
                     'league_season', # active season for leagues
                     'league_create_season'] # flips in December
     state_df = pd.DataFrame([{k: response.json()[k] for k in keys_subset}])
-    
-    
 
 
     ######################
     #        LOAD        #
     ######################
     # Write state dataframe to postgres db
+    state_df.to_sql(name='sleeper_state', con=engine, schema = 'sleeper', if_exists = 'replace', index=False)
+    #engine.dispose()
 
 
-
+if __name__ == "__main__":
+    main()
