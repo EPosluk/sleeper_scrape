7b6e532e2f851f5bc0d7da1bd1ad238596fb32ec
eposluk <52364323+EPosluk@users.noreply.github.com>
2025-01-01 21:23:02 -0500

    Sleeper_state: Added data types before writing to postgres db

--- src/sleeper_state.py
+++ src/sleeper_state.py
@@ -2,7 +2,8 @@ import requests
 import pandas as pd
 import psycopg
 import config
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Date
+from sqlalchemy.types import INTEGER, Float, VARCHAR
 
 
 def main():
@@ -25,22 +26,23 @@ def main():
     #     TRANSFORM      #
     ######################
     # Extract data of interest using subset of key/value pairs
-    keys_subset = ['week', # week
-                    'season', # current season
-                    'season_type', # pre, post, regular
-                    'season_start_date', # regular season start
-                    'leg', # week of regular season
-                    'league_season', # active season for leagues
-                    'league_create_season'] # flips in December
-    state_df = pd.DataFrame([{k: response.json()[k] for k in keys_subset}])
+    keys_subset = {'week': INTEGER(), # week
+                    'season': INTEGER(), # current season
+                    'season_type': VARCHAR(length=255), # pre, post, regular
+                    'season_start_date': Date, # regular season start
+                    'leg': INTEGER(), # week of regular season
+                    'league_season': INTEGER(), # active season for leagues
+                    'league_create_season': INTEGER()} # flips in December
+    state_df = pd.DataFrame([{k: response.json()[k] for k in keys_subset.keys()}])
 
 
     ######################
     #        LOAD        #
     ######################
     # Write state dataframe to postgres db
-    state_df.to_sql(name='sleeper_state', con=engine, schema = 'sleeper', if_exists = 'replace', index=False)
-    #engine.dispose()
+    state_df.to_sql(name='sleeper_state', con=engine, schema = 'sleeper', if_exists = 'replace', index=False,
+                    dtype=keys_subset)
+    engine.dispose()
 
 
 if __name__ == "__main__":
