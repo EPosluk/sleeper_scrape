91668d4a1b0d08a1ea7c82877fb703def1a01d91
eposluk <52364323+EPosluk@users.noreply.github.com>
2025-01-05 20:55:58 -0500

    sleeper_players creation and writing to postgres db

--- src/sleeper_players.py
+++ src/sleeper_players.py
@@ -1,11 +1,42 @@
-import requests
 import pandas as pd
-import json
+import requests 
+import psycopg
+import config
+from sqlalchemy import create_engine, Date
+from sqlalchemy.types import INTEGER, VARCHAR, BOOLEAN
 
 def main():
     ######################
     #       SET UP       #
     ######################
+    # SQL Alchemy engine creation
+    engine = create_engine(config.connection_string)
+    keys_subset = {'search_full_name': VARCHAR(),
+        'weight': VARCHAR(),
+        'years_exp': INTEGER(),
+        'search_last_name': VARCHAR(),
+        'active': BOOLEAN(),
+        'last_name': VARCHAR(),
+        'injury_body_part': VARCHAR(),
+        'first_name': VARCHAR(),
+        'depth_chart_position': VARCHAR(),
+        'depth_chart_order': INTEGER(),
+        'status': VARCHAR(),
+        'age': INTEGER(),
+        'full_name': VARCHAR(),
+        'injury_status': VARCHAR(),
+        'search_rank': INTEGER(),
+        'search_first_name': VARCHAR(),
+        'birth_date': Date(),
+        'college': VARCHAR(),
+        'high_school': VARCHAR(),
+        'height': VARCHAR(),
+        'position': VARCHAR(),
+        'stats_id': INTEGER(),
+        'fantasy_positions': VARCHAR(),
+        'player_id': VARCHAR(),
+        'team': VARCHAR(),
+        'number': INTEGER()}
 
 
     ######################
@@ -20,15 +51,17 @@ def main():
     #     TRANSFORM      #
     ######################
     # Flatten nested dictionary to a dataframe
-    json_data = json.loads(response.content)
-    players = [{**{'player_id':k1}, **v1} for k1, v1 in json_data.items()]
-    players_df = pd.DataFrame(players)
+    players_df = pd.DataFrame([{k: v for k, v in e.items() if k in keys_subset.keys()} for d, e in response.json().items()])
 
 
     ######################
     #        LOAD        #
     ######################
     # Write players dataframe to postgres db
+    players_df.to_sql(name='sleeper_players', con=engine, schema = 'sleeper', if_exists = 'replace', index=False,
+                dtype=keys_subset)
+    engine.dispose()
 
 
-
+if __name__ == "__main__":
+    main()
